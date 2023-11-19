72594b70158f0d14df89adc802ef3decfed02ec6
vstarzf <vstarzf@gmail.com>
2023-11-19 00:04:59 -0500

    Implements fragments in main activity

--- app/src/main/java/com/example/dreamsleepapp/MainActivity.kt
+++ app/src/main/java/com/example/dreamsleepapp/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.dreamsleepapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// testing
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavBar : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeItem -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, HomeFragment.newInstance("","")).commit()
+                }
+                R.id.histItem -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, SleepHistoryFragment.newInstance("","")).commit()
+
+                }
+                R.id.statsItem -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, DreamFragment.newInstance("", "")).commit()
+                }
+            }
+
+            true
+        }
     }
 }
